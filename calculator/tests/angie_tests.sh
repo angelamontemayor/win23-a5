03280a18a99b60910f3f1107f05a9aa11387f1c7
Angela Montemayor <angelamontemayor@me.com>
2023-02-22 23:04:57 -0800

    makefile and tests

---
+++ calculator/tests/angie_tests.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# (The absolute path to the program is provided as the first and only argument.)
+CALCULATOR=$1
+
+echo "We've set up a GitHub Actions Workflow that will run all"
+echo "of the shell scripts in this directory as a series of tests."
+echo
+echo "To fail any test, you should use the exit 1 command;"
+echo "To end a test early as a success, you should use the exit 0 command."
+
+if ! $CALCULATOR 100 * 2; then
+  echo 'ERROR! A valid run of the calculator (100 * 2) failed!'
+  exit 1
+fi
+
+if ! $CALCULATOR 100 / 2; then
+  echo 'ERROR! A valid run of the calculator (100 / 2) failed!'
+  exit 1
+fi
+
+if ! $CALCULATOR 100 - 2; then
+  echo 'ERROR! A valid run of the calculator (100 - 2) failed!'
+  exit 1
+fi
+
+if ! $CALCULATOR 100 + 2; then
+  echo 'ERROR! A valid run of the calculator (100 + 2) failed!'
+  exit 1
+fi
+
+if [[ $($CALCULATOR 6 / 2) -ne 3 ]];
+  echo 'ERROR! A valid run of the calculator (6 / 2) failed to produce 3 as an output!'
+  exit 1
+fi
+
+if $CALCULATOR 3 ^ 2; then
+  echo 'ERROR! An invalid run of the application (3 ^ 2) apparently succeeded?!'
+  exit 1
+fi
